87dda76f1760fd6841eb770b65e33f597b821ed1
krivorotovss <s.krivorotov@yandex.ru>
2022-01-23 18:37:47 +0700

    Add Range project

---
+++ Range/src/ru/academits/krivorotov/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.krivorotov.range;
+
+public class Range {
+}
---
+++ Range/src/ru/academits/krivorotov/range_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.krivorotov.range_main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
