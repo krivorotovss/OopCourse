b38dcd4863e67cbf891ee119ca54c9f0b25f8335
krivorotovss <s.krivorotov@yandex.ru>
2022-01-24 00:22:54 +0700

    filled in Range project

---
+++ Range/src/ru/academits/krivorotov/range/Main.java
@@ -0,0 +1,35 @@
+package ru.academits.krivorotov.range;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите вещественное число:");
+        double userNumber = scanner.nextDouble();
+
+        double from = 2.5;
+        double to = 10.2;
+
+        Range range = new Range(from, to);
+
+        if (range.isInside(userNumber)) {
+            System.out.printf("Число %.2f попадает в указанный диапазон%n", userNumber);
+        } else {
+            System.out.println("Число вне диапазона");
+            return;
+        }
+
+        System.out.printf("Длина диапазона чисел = %.2f%n", range.getLength());
+
+        from = 5;
+        to = 18;
+
+        range.setFrom(from);
+        range.setTo(to);
+
+        System.out.println("Новые значения: from = " + range.getFrom() + ", to = " + range.getTo());
+
+        System.out.printf("Новая длина диапазона чисел = %.2f%n", range.getLength());
+    }
+}
--- Range/src/ru/academits/krivorotov/range_main/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.academits.krivorotov.range_main;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
