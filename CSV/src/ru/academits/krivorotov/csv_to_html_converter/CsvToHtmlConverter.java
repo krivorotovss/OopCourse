373f0c0150eef24a07adfa40b74c1fec2c0e9f46
krivorotovss <s.krivorotov@yandex.ru>
2022-08-26 20:51:10 +0700

    Fixed CSV project

--- CSV/src/ru/academits/krivorotov/csv_to_html_converter/CsvToHtmlConverter.java
+++ CSV/src/ru/academits/krivorotov/csv_to_html_converter/CsvToHtmlConverter.java
@@ -1,63 +1,83 @@
 package ru.academits.krivorotov.csv_to_html_converter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class CsvToHtmlConverter {
-    public void convertAndWriteHtmlFile(BufferedReader reader, PrintWriter writer) throws IOException {
-        writer.println("<table>");
+    public void convertAndWriteHtmlFile(String pathToInputFile, String pathToOutputFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathToInputFile));
+             PrintWriter writer = new PrintWriter(new FileWriter(pathToOutputFile))) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html lang=\"ru\">");
+            writer.println("\t" + "<head>");
+            writer.println("\t\t" + "<meta charset=\"UTF-8\" />");
+            writer.println("\t\t" + "<title>Страница</title>");
+            writer.println("\t" + "<head>");
+            writer.println("\t" + "<body>");
+            writer.println("\t\t" + "<table>");
 
-        String line;
+            String line;
 
-        boolean isDataInQuotes = false;
+            boolean isDataInQuotes = false;
 
-        while ((line = reader.readLine()) != null) {
-            if (!isDataInQuotes) {
-                writer.println("\t" + "<tr>");
-                writer.write("\t\t" + "<td>");
-            }
-
-            for (int i = 0; i < line.length(); ++i) {
-                char symbol = line.charAt(i);
-
-                if (symbol == '"') {
-                    if (!isDataInQuotes) {
-                        isDataInQuotes = true;
-                    } else {
-                        if (i != line.length() - 1 && line.charAt(i + 1) == '"') {
-                            writer.write("\"");
-                            i++;
-                        } else {
-                            isDataInQuotes = false;
-                        }
+            while ((line = reader.readLine()) != null) {
+                if (!isDataInQuotes) {
+                    if (line.length() == 0) {
+                        continue;
                     }
-                } else if (symbol == ',') {
-                    if (!isDataInQuotes) {
-                        writer.println("</td>");
-                        writer.write("\t\t" + "<td>");
+
+                    writer.println("\t\t\t" + "<tr>");
+                    writer.write("\t\t\t\t" + "<td>");
+                }
+
+                for (int i = 0; i < line.length(); ++i) {
+                    char symbol = line.charAt(i);
+
+                    if (symbol == '"') {
+                        if (!isDataInQuotes) {
+                            isDataInQuotes = true;
+                        } else {
+                            if (i != line.length() - 1 && line.charAt(i + 1) == '"') {
+                                writer.write("\"");
+                                i++;
+                            } else {
+                                isDataInQuotes = false;
+                            }
+                        }
+                    } else if (symbol == ',') {
+                        if (!isDataInQuotes) {
+                            writer.println("</td>");
+                            writer.write("\t\t\t\t" + "<td>");
+                        } else {
+                            writer.write(symbol);
+                        }
+                    } else if (symbol == '>') {
+                        writer.write("&gt;");
+                    } else if (symbol == '<') {
+                        writer.write("&lt;");
+                    } else if (symbol == '&') {
+                        writer.write("&amp;");
                     } else {
                         writer.write(symbol);
                     }
-                } else if (symbol == '>') {
-                    writer.write("&gt;");
-                } else if (symbol == '<') {
-                    writer.write("&lt;");
-                } else if (symbol == '&') {
-                    writer.write("&amp;");
+                }
+
+                if (!isDataInQuotes) {
+                    writer.println("</td>");
+                    writer.println("\t\t\t" + "</tr>");
                 } else {
-                    writer.write(symbol);
+                    writer.write("<br/>");
                 }
             }
 
-            if (!isDataInQuotes) {
-                writer.println("</td>");
-                writer.println("\t" + "</tr>");
-            } else {
-                writer.write("<br/>");
-            }
+            writer.println("\t\t" + "</table>");
+            writer.println("\t" + "</body>");
+            writer.print("</html>");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла");
+        } catch (Exception e) {
+            System.out.println("Ошибка");
         }
-
-        writer.println("</table>");
     }
 }
--- CSV/src/ru/academits/krivorotov/csv_to_html_converter_main/Main.java
+++ CSV/src/ru/academits/krivorotov/csv_to_html_converter_main/Main.java
@@ -2,21 +2,12 @@ package ru.academits.krivorotov.csv_to_html_converter_main;
 
 import ru.academits.krivorotov.csv_to_html_converter.CsvToHtmlConverter;
 
-import java.io.*;
-
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("CSV/src/example.txt"));
-             PrintWriter writer = new PrintWriter(new FileWriter("CSV/src/output.txt"))) {
-            CsvToHtmlConverter converter = new CsvToHtmlConverter();
-            converter.convertAndWriteHtmlFile(reader, writer);
+        String pathToInputFile = "CSV/src/example.csv";
+        String pathToOutputFile = "CSV/src/output.html";
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден");
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла");
-        } catch (Exception e) {
-            System.out.println("Ошибка");
-        }
+        CsvToHtmlConverter converter = new CsvToHtmlConverter();
+        converter.convertAndWriteHtmlFile(pathToInputFile, pathToOutputFile);
     }
 }
